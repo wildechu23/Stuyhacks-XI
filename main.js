3cd8c79efef2e4c577e904e37111424d5314f368
Wilde Chu <wildechu23@gmail.com>
2022-01-15 16:03:31 -0500

    fetus

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     // put setup code here
+    frameRate(60);
+    background(0);
 }
 
 function draw() {
     // put drawing code here
-    background("red");
+
 }
